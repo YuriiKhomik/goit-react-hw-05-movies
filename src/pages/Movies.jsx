9b6e1f97f0eb2e464bb6bbc868eec9853cb9df79
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-03-17 22:37:02 +0200

    ...

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useSearchParams, Outlet } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { Box } from 'components/Box/Box';
 import { SearchForm } from 'components/SearchForm';
 import { searchMovies } from 'services';
